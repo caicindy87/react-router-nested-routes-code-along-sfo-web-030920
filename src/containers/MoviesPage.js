05fc324c4f8b29dc78beb64d271ba0124d7056be
caicindy87 <caicindy87@gmail.com>
2020-05-21 21:05:51 -0700

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,25 @@
 // .src/containers/MoviesPage.js
-import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
+import React from "react";
+import { Route } from "react-router-dom";
 
+import MoviesList from "../components/MovieList";
+import MovieShow from "../components/MovieShow";
+
+// Here we add 'match' to the arguments so we can access the path info in `routerProps` that is passed from App.js as a prop
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    {/* We also add a `Route` component that will render `MovieShow` when a movie is selected */}
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Choose a movie from the list above</h3>}
+    ></Route>
+    <Route
+      path={`${match.url}/:movieID`}
+      render={(routerProps) => <MovieShow {...routerProps} movies={movies} />}
+    ></Route>
   </div>
-)
+);
 
-export default MoviesPage
+export default MoviesPage;
